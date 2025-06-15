78c3c5242c1c8eee652c2595608ba7f211783d0c
TobinPalmer <tobinkpalmer@gmail.com>
2025-06-14 17:19:48 -0700

    fix loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,21 @@
 #include <chrono>
 #include <thread>
 
+Robot robot;
+auto lastTime = std::chrono::steady_clock::now();
+
 void setup()
 {
+  robot.begin();
 }
 
 void loop()
 {
-}
+  auto now = std::chrono::steady_clock::now();
+  float dt = std::chrono::duration<float>(now - lastTime).count();
+  lastTime = now;
 
-int main()
-{
-  Robot robot;
-  robot.begin();
+  robot.update(dt);
 
-  auto lastTime = std::chrono::steady_clock::now();
-
-  while (true)
-  {
-    auto now = std::chrono::steady_clock::now();
-    float dt = std::chrono::duration<float>(now - lastTime).count();
-    lastTime = now;
-
-    robot.update(dt);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1)); // 1000 Hz loop
-  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(1)); // 1000 Hz loop
 }
